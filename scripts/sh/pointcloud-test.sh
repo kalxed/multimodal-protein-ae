37afb73adbc51aed8f8903dfa6e75d6b525fde0e
peter-howell <44275653+peter-howell@users.noreply.github.com>
2024-12-05 17:42:35 -0500

    add scipt to test PAE

---
+++ scripts/sh/pointcloud-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J TRAIN-POINTCLOUD
+#SBATCH -n 1
+#SBATCH --gpus-per-task=1
+#SBATCH -o logs/test-pae.out
+#SBATCH --mem=16G
+
+echo "Slurm job id: $SLURM_JOB_ID"
+
+source ~/miniconda3/bin/activate dlprotproj
+
+python scripts/py/pretrain_pae.py --mode test --epochs 200 --model-path models/pae-200.pt
+
+echo "Done"
+
+exit 0
