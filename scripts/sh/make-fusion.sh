69f56cb17e361b4aa0d56245a44d2d3eaab3dea3
peter-howell <44275653+peter-howell@users.noreply.github.com>
2024-12-05 11:45:32 -0500

    rename fusion_cae script

--- scripts/sh/make-fusion.sh
+++ scripts/sh/make-fusion.sh
@@ -8,7 +8,7 @@ echo "Slurm job id: $SLURM_JOB_ID"
 
 source ~/miniconda3/bin/activate dlprotproj
 
-python scripts/py/encode_fuse.py --mode process --pae-path models/PAE-200.pt
+python scripts/py/fusion_cae.py --mode process --pae-path models/PAE-200.pt
 
 echo "Done."
 
