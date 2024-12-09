14c12997941001d0ce54779f1551cf6c57d1e336
peter-howell <44275653+peter-howell@users.noreply.github.com>
2024-12-09 12:03:21 -0500

    change log file, and specify location of attention model

--- scripts/sh/pretrain-fusion-cae.sh
+++ scripts/sh/pretrain-fusion-cae.sh
@@ -2,13 +2,13 @@
 #SBATCH -J CAE-FUSION-TRAIN
 #SBATCH -n 1
 #SBATCH --gpus-per-task=1
-#SBATCH -o logs/cae-pretrain-%j.out
+#SBATCH -o logs/cae-attention-pretrain.out
 
 echo "Slurm job id: $SLURM_JOB_ID"
 
 source ~/miniconda3/bin/activate dlprotproj
 
-python scripts/py/fusion_cae.py --mode train --pae-path models/PAE-200.pt --id-file fused-proteins
+python scripts/py/fusion_cae.py --mode train --pae-path models/PAE-200.pt --id-file fused-proteins --model-path models/CAE-ATTENTION.pt
 
 echo "Done training"
 
--- scripts/sh/test-fusion-cae.sh
+++ scripts/sh/test-fusion-cae.sh
@@ -2,13 +2,13 @@
 #SBATCH -J CAE-FUSION-TEST
 #SBATCH -n 1
 #SBATCH --gpus-per-task=1
-#SBATCH -o logs/test-fusion-%j.out
+#SBATCH -o logs/test-attention-fusion.out
 
 echo "Slurm job id: $SLURM_JOB_ID"
 
 source ~/miniconda3/bin/activate dlprotproj
 
-python scripts/py/fusion_cae.py --mode test --pae-path models/PAE-200.pt --id-file fused-proteins
+python scripts/py/fusion_cae.py --mode test --pae-path models/PAE-200.pt --id-file fused-proteins --model-path models/CAE-ATTENTION.pt
 
 echo "Done Testing"
 
