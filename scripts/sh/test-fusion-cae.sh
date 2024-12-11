98ca149e59a485040c3d70cfbc55d62f73f165bb
peter-howell <44275653+peter-howell@users.noreply.github.com>
2024-12-11 14:55:18 -0500

    change to not use the concrete distribution

--- scripts/sh/test-fusion-cae.sh
+++ scripts/sh/test-fusion-cae.sh
@@ -2,13 +2,13 @@
 #SBATCH -J CAE-FUSION-TEST
 #SBATCH -n 1
 #SBATCH --gpus-per-task=1
-#SBATCH -o logs/test-attention-fusion.out
+#SBATCH -o logs/test-attention-fusion-100.out
 
 echo "Slurm job id: $SLURM_JOB_ID"
 
 source ~/miniconda3/bin/activate dlprotproj
 
-python scripts/py/fusion_cae.py --mode test --pae-path models/PAE-200.pt --id-file fused-proteins --model-path models/CAE-ATTENTION.pt
+python scripts/py/fusion_cae.py --mode test --pae-path models/PAE-200.pt --id-file fused-proteins --model-path models/CAE-ATTENTION-100.pt
 
 echo "Done Testing"
 
