d0655e8e66b0416dd28d570d80303246d7648ed6
peter-howell <44275653+peter-howell@users.noreply.github.com>
2024-12-11 19:35:04 -0500

    use attention, change name

--- scripts/sh/pretrain-fusion-cae.sh
+++ scripts/sh/pretrain-fusion-cae.sh
@@ -8,7 +8,7 @@ echo "Slurm job id: $SLURM_JOB_ID"
 
 source ~/miniconda3/bin/activate dlprotproj
 
-python scripts/py/fusion_cae.py --mode train --pae-path models/PAE-200.pt --id-file fused-proteins --model-path models/CAE-ATTENTION.pt
+python scripts/py/fusion_cae.py --mode train --pae-path models/PAE-200.pt --id-file fused-proteins --model-path models/CAE-attention-concrete.pt --attention True
 
 echo "Done training"
 
