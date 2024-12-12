c7e819aa207d60102202b65d427d0defb49d1226
peter-howell <44275653+peter-howell@users.noreply.github.com>
2024-12-11 21:48:09 -0500

    better structure

--- mpae/nn/__init__.py
+++ mpae/nn/__init__.py
@@ -1,12 +1,8 @@
-from .vgae import VariationalGCNEncoder
-from .attention import AttentionFusion
 
-from .concrete_autoencoder import ConcreteAutoencoder
-
-__all__ = ["AttentionFusion",
-           "ConcreteAutoencoder",
-           "esm",
-           "pae",
-           "VariationalGCNEncoder",
-           ]
+__all__ = [
+    "cae",
+    "esm",
+    "pae",
+    "vgcn",
+]
 
