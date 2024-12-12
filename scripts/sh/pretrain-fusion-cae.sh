5e0e665dc641d671b49655ef339790a667521c46
peter-howell <44275653+peter-howell@users.noreply.github.com>
2024-12-11 19:31:41 -0500

    use attention

--- scripts/sh/pretrain-fusion-cae.sh
+++ scripts/sh/pretrain-fusion-cae.sh
@@ -8,7 +8,7 @@ echo "Slurm job id: $SLURM_JOB_ID"
 
 source ~/miniconda3/bin/activate dlprotproj
 
-python scripts/py/fusion_cae.py --mode train --pae-path models/PAE-200.pt --id-file fused-proteins --model-path models/CAE-ATTENTION-100.pt --epochs 100
+python scripts/py/fusion_cae.py --mode train --pae-path models/PAE-200.pt --id-file fused-proteins --model-path models/CAE-ATTENTION-100.pt --epochs 100 --attention True
 
 echo "Done training"
 
